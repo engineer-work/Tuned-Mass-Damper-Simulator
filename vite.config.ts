3a144a3156aa2c395498f286fcb36998fd4f934d
engineer-work <gobalkrishnan.work@gmail.com>
2025-12-15 03:02:33 +0530

    working code

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/Tuned-Mass-Damper-Simulator/',
       server: {
         port: 3000,
         host: '0.0.0.0',
